500df9fa369d39f99ee768e3fa7bfddfc513883f
muhammad_nubli_zulfahmi <mnzulfahmi@student.telkomuniversity.ac.id>
2021-03-10 21:19:11 +0700

    Menambahkan gambar pada list

--- app/src/main/java/org/d3if3087/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if3087/galerihewan/Hewan.kt
@@ -1,4 +1,8 @@
 package org.d3if3087.galerihewan
 
-data class Hewan(val nama: String, val namaLatin: String)
+data class Hewan(
+    val nama: String,
+    val namaLatin: String,
+    val imageResId: Int
+)
 
