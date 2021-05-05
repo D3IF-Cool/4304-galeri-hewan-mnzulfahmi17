c9cd1ed9aa1e401b958f8f3197da4d1281423c2b
Muhammad Nubli Zulfahmi <mnzulfahmi@student.telkomuniversity.ac.id>
2021-05-05 21:47:26 +0700

    3.4.Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if3087/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if3087/galerihewan/model/Hewan.kt
@@ -1,8 +1,11 @@
 package org.d3if3087.galerihewan.model
 
+import org.d3if3087.galerihewan.R
+
 data class Hewan(
         val nama: String,
         val namaLatin: String,
-        val imageResId: Int
+//        val imageResId: Int
+        val imageResId: Int = R.drawable.angsa
 )
 
